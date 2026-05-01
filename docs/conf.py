ee3b2729d459ee2b0afeb48499d56c4950e8d999
Shubham Shinde <shubham57shinde57@gmail.com>
2026-05-01 12:57:42 +0100

    Add Sphinx extensions and update theme options

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,18 @@ html_theme = 'sphinx_rtd_theme'
 
 html_logo = "../logo/qens_logo_dark.png"
 
+
+
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon"
+]
+
 html_theme_options = {
     "logo_only": True,
     "display_version": False,
+    "navigation_depth": 4,
+    "collapse_navigation": False,
+    "sticky_navigation": True,
 }
